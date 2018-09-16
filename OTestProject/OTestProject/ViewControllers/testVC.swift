8b9a4f9dd1ffae19bd3b55ca06cf1c569b729ed9
SomeGoodMan <marat31bv@gmail.com>
2018-09-16 17:55:38 +0600

    Tested getting albums

--- OTestProject/OTestProject/ViewControllers/testVC.swift
+++ OTestProject/OTestProject/ViewControllers/testVC.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import GameplayKit
 
 class TestVC : UIViewController {
     
@@ -23,7 +24,25 @@ class TestVC : UIViewController {
     }
     
     func isGettingAlbums(result : [Album]) {
+        
         print("-----------ALBUMS------------")
+        
+        var objectsMassive : [Album] = []
+        var randomNumberOfElementsOnScreen = Int(arc4random_uniform(14)) + 1
+        
+        while randomNumberOfElementsOnScreen < 10 {
+            randomNumberOfElementsOnScreen = Int(arc4random_uniform(14)) + 1
+        }
+        
+        while objectsMassive.count < randomNumberOfElementsOnScreen {
+            let randomIndexOfElement = Int(arc4random_uniform(UInt32(result.count)))
+            if !(containsInAlbumArray(array: objectsMassive, object: result[randomIndexOfElement])){
+                
+            objectsMassive.append(result[randomIndexOfElement])
+                print("username : \(result[randomIndexOfElement].title!)")
+            }
+        }
+        
         for a in result {
             print(a.title!)
         }
@@ -38,6 +57,15 @@ class TestVC : UIViewController {
         }
         
     }
-    
-    
+    func containsInAlbumArray (array : [Album], object : Album) -> Bool {
+        
+        for album in array {
+            if album.id == object.id {
+                return true
+                break
+            }
+        }
+        return false
+        
+    }
 }
